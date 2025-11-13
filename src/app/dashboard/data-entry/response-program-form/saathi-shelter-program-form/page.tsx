68ecc1deb8057adde3f90e65121699fb1665bdae
rodinshrestha <rodin@kumo-labs.com>
2025-11-13 11:54:53 +0545

    fix: unfinished saathi form module

---
+++ src/app/dashboard/data-entry/response-program-form/saathi-shelter-program-form/page.tsx
@@ -0,0 +1,7 @@
+import SaathiShelterProgramForm from "@/modules/saathi-shelter-program-form";
+
+const SaathiShelterProgramFormPage = () => {
+  return <SaathiShelterProgramForm />;
+};
+
+export default SaathiShelterProgramFormPage;
---
+++ src/modules/saathi-shelter-program-form/index.tsx
@@ -0,0 +1,65 @@
+"use client";
+import React from "react";
+
+import { useFormik } from "formik";
+
+import { SaathiShelterProgramFormType } from "./saathi-shelter.types";
+import { StyledDiv } from "./style";
+
+const SaathiShelterProgramForm = () => {
+  const formik = useFormik<SaathiShelterProgramFormType>({
+    initialValues: {
+      name: "",
+      date_of_entry: "",
+      code_no: "",
+      age_of_survivor: "",
+      number_of_dependent_children: "",
+      dependent_age_gender: "",
+      province: "",
+      district: "",
+      enthnicity: "",
+      type_of_violence: "",
+      marital_status: "",
+      pregnancy_status: "",
+      disability_status: "",
+      impact_of_violence: "",
+      reason_for_comming_to_shelter: "",
+      perpetrator_name: "",
+      number_of_perpetrators: "",
+      age: "",
+      education: "",
+      occupation: "",
+      politically_socially_powerful: "",
+      substance_use: "",
+      charged_or_arrested: "",
+      physical_status: "",
+      mental_status: "",
+      referred_form: "",
+      referred_to_by_saathi: "",
+      referred_date_of_entry: "",
+      date_of_discharge: "",
+      duration_range: "",
+      service_provided: "",
+      first_time_in_shelter: "",
+      previous_stay_duration: "",
+      immediate_support: "",
+      long_term_support: "",
+      reintegration_status: "",
+      family_reintegration: "",
+      reintegration_new_community: "",
+      counseling_via_phone: "",
+      counseling_to_police: "",
+      current_status_of_survivor_and_dependents: "",
+      other_remarks: "",
+    },
+    onSubmit: () => {
+      //
+    },
+  });
+
+  console.log(formik);
+
+  return <StyledDiv>SaathiShelterProgramForm</StyledDiv>;
+};
+
+export default SaathiShelterProgramForm;
